d7579d3c08530c3286c18e25f780a6f067f4b018
apexa199 <apexaajudiya9@gmail.com>
2023-11-07 18:48:11 +0530

    task

--- task-13 drag-drop/script.js
+++ task-13 drag-drop/script.js
@@ -114,19 +114,5 @@ const dispaly = () => {
     })
     document.getElementById("dis").innerHTML = a.join("");
 }
-dispaly()
+dispaly();
 
-
-function allowDrop(ev) {
-  ev.preventDefault();
-}
-
-function drag(ev) {
-  ev.dataTransfer.setData("text", ev.target.id);
-}
-
-function drop(ev) {
-  ev.preventDefault();
-  var data = ev.dataTransfer.getData("text");
-  ev.target.appendChild(document.getElementById(data));
-}

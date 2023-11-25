a4f6a73533c0cf6eb4f7c1544976ae954b2f5a8b
apexa199 <apexaajudiya9@gmail.com>
2023-11-25 12:18:17 +0530

    task

---
+++ React/01basic/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import MultipleState from './MultipleState';
+import { Counter } from './Home';
+import { APIDisplay } from './APIDisplay';
+import { FormFunction } from './FormFunction';
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    
+    <FormFunction/>  
+
+
+  </React.StrictMode>
+);
+
